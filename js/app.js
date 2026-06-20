02606bd3b76553529b0eadfbf1f67e8c002dc671
ldanielalopez <ldanielalopezo@gmail.com>
2026-06-20 13:39:29 -0500

    Estructura inicial del portafolio

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Menú hamburguesa
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    });
